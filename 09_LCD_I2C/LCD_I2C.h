cfe287aadab513a385c118039feaf48aac57c73f
TM-khoa <minhkhoa9a31314@gmail.com>
2022-11-17 19:43:37 +0700

    Basic LCD driver

--- 09_LCD_I2C/LCD_I2C.h
+++ 09_LCD_I2C/LCD_I2C.h
@@ -27,38 +27,15 @@ using namespace std;
 #define LCD_PCF8574_D7          6
 #define LCD_PCF8574_BACKLIGHT   7
 
-struct PCF8574_conf_t{
-    gpio_num_t scl_pin;
-    gpio_num_t sda_pin;
-    i2c_port_t port;
-    uint8_t Address;
-};
-
-struct LCD_conf_t{
-    uint8_t d4;
-    uint8_t d5;
-    uint8_t d6;
-    uint8_t d7;
-    uint8_t e;
-    uint8_t rs;
-    uint8_t backlight;
-    hd44780_font_t font;
-    uint8_t lines;
-    bool IsHD44780;
-};
-
 class ClassLCDI2C{
 private:
     
 public:
-    
     esp_err_t begin();
     esp_err_t clear();
     esp_err_t TurnOnBackLight();
     esp_err_t TurnOffBackLight();
     esp_err_t print(const char *S, uint8_t x, uint8_t y);
-    // esp_err_t configPin(LCD_conf_t *l,PCF8574_conf_t *pcf);
-    // esp_err_t pLCD_cb(const hd44780_t *lcd, uint8_t data);
 };
 
 
